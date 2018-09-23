358acb4dd2c948e5529ffef62aba11feb01c5050
John  Tucker <john@larkintuckerllc.com>
2018-09-23 14:22:45 -0400

    application

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(cors());
+
+app.get('/', (req, res) => res.send({ hello: 'world' }));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
