bd93387114c43da2fbf47ee3f4a73effdf79df1b
John  Tucker <john@larkintuckerllc.com>
2018-09-24 04:33:59 -0400

    Secure API

--- api/index.js
+++ api/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
 const app = express();
 const port = 8080;
 
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://YOUR_AUTH0_DOMAIN/.well-known/jwks.json`,
+  }),
+  audience: '{YOUR_API_IDENTIFIER}',
+  issuer: 'https://YOUR_AUTH0_DOMAIN/',
+  algorithms: [ 'RS256' ],
+});
+
 app.use(cors());
 
-app.get('/', (req, res) => res.send({ hello: 'world' }));
+app.get('/', checkJwt, (req, res) => res.send({ hello: 'world' }));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
